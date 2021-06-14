4cec4acc3093482342cf1655418666f0165ec68a
Sreekumar <73136964+sakhaavvaavaj93@users.noreply.github.com>
2021-06-14 21:09:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -60,10 +60,10 @@ async def start(client, message):
     ]
     ]
     reply_markup = InlineKeyboardMarkup(buttons)
-    await message.reply(HOME_TEXT.format(message.from_user.first_name, message.from_user.id), reply_markup=reply_markup)
-
-
-
+    await message.reply_text(
+    HOME_TEXT.format(message.from_user.first_name, message.from_user.id), 
+    reply_markup=reply_markup
+    )
 @Client.on_message(filters.command("help"))
 async def show_help(client, message):
     buttons = [
